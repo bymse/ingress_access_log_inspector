03fe36fc0ed688c6b3ba625dd092553a0419b9b0
bymse <egorstrelov@gmail.com>
2024-03-03 21:58:31 +0300

    Close cursor after request

--- storage/__init__.py
+++ storage/__init__.py
@@ -17,11 +17,10 @@ def ensure_connected(host, port, dbname, user, password):
 
 
 def save_log(log_data):
-    cursor = __connection.cursor()
-
-    cursor.execute("""
-        INSERT INTO access_logs (request_id, host, client_ip, time, method, path, query, status, referer, user_agent)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """, (log_data['request_id'], log_data['host'], log_data['client_ip'], log_data['time'],
-          log_data['method'], log_data['path'], log_data['query'], log_data['status'],
-          log_data['referer'], log_data['user_agent']))
+    with __connection.cursor() as cursor:
+        cursor.execute("""
+            INSERT INTO access_logs (request_id, host, client_ip, time, method, path, query, status, referer, user_agent)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """, (log_data['request_id'], log_data['host'], log_data['client_ip'], log_data['time'],
+                  log_data['method'], log_data['path'], log_data['query'], log_data['status'],
+                  log_data['referer'], log_data['user_agent']))
